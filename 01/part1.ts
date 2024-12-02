68a5d45a10a51185b173b1c989248b5d97f8df70
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2024-12-02 18:53:02 +0000

    comments

--- 01/part1.ts
+++ 01/part1.ts
@@ -11,18 +11,21 @@ for (const line of lines) {
         continue;
     }
 
+    // collect parsed ints into lists
     const [left_str, right_str] = line.split("   ");
 
     left.push(Number.parseInt(left_str));
     right.push(Number.parseInt(right_str))
 }
 
+// need to define a comparator to sort numbers properly in js
 // :laughatthisuser:
 const num_comparator = (a: number, b: number) => a - b;
 
 left.sort(num_comparator);
 right.sort(num_comparator);
 
+// tally distances
 let dist_sum = 0;
 for (const idx in left) {
     dist_sum += Math.abs(left[idx] - right[idx]);
